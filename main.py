cb4e637eb9962b0322dea328fdaddce105c45be3
khanya-mcfadden <23malgasmk037@collyers.ac.uk>
2024-10-18 10:16:11 +0100

    added files

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os 
+from website import create_app
 
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'mysecretkey'
-    if os.getnev("database_url"):
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("database_url")
-    else:
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{DB_NAME}'
-    
-    
-    
-# old code
-# @app.route('/')
-# def index():
-#     return 'Hello World.'
+app = create_app()
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
